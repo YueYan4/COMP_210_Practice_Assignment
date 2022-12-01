ae33f6c7e629253288271c15495aa08043b773e5
YueYan4 <yueyan@email.unc.edu>
2022-12-01 12:41:54 -0500

    updated

--- Heaps/HeapImpl.java
+++ Heaps/HeapImpl.java
@@ -2,17 +2,20 @@ public class HeapImpl implements Heap{
 
     private int[] elts;
     private int max;
+    private int size;
 
     /*Define the max */
     public HeapImpl(int max) {
         this.max = max;
         this.elts = new int[max];
+        size = 0;
     }
 
     /*Default constructor */
     public HeapImpl() {
         this.max = 100;
         this.elts = new int[max];
+        size = 0;
     }
 
     /*Gets the heap array
